17b2f761ca4d6408b03398063687664a3b5c1b7c
capricorn <8250820+capricorn@users.noreply.github.com>
2023-12-28 20:54:25 -0700

    Import generator code from playground

--- Sources/TersePG/TersePG.swift
+++ Sources/TersePG/TersePG.swift
@@ -1,2 +1,72 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+typealias PrefixAutomata = (String?) -> String?
+
+postfix operator *
+infix operator |: AdditionPrecedence
+infix operator >: MultiplicationPrecedence
+
+func P(_ char: Character) -> PrefixAutomata {
+    { input in
+        guard let input else {
+            return nil
+        }
+        
+        if input.isEmpty {
+            return nil
+        }
+        
+        if input.first == char {
+            return String(input.suffix(input.count-1))
+        }
+        
+        return nil
+    }
+}
+
+func R(_ body: @escaping (@escaping PrefixAutomata, String?) -> String?) -> PrefixAutomata {
+    var f: PrefixAutomata!
+    f = { (input: String?) in
+        body(f, input)
+    }
+    
+    return f
+}
+
+postfix func *(_ auto: @escaping PrefixAutomata) -> PrefixAutomata {
+    { input in
+        guard let input else {
+            return nil
+        }
+        
+        var result: String? = input
+        while (auto(result) != nil) {
+            result = auto(result)
+        }
+        
+        return result
+    }
+}
+
+func |(_ a1: @escaping PrefixAutomata, _ a2: @escaping PrefixAutomata) -> PrefixAutomata {
+    { input in
+        guard let input else {
+            return nil
+        }
+        
+        return a1(input) ?? a2(input)
+    }
+}
+
+// Compose a2(a1(s))
+func >(_ a1: @escaping PrefixAutomata, _ a2: @escaping PrefixAutomata) -> PrefixAutomata {
+    { input in
+        guard let input else {
+            return nil
+        }
+        
+        guard let a1Result = a1(input) else {
+            return nil
+        }
+        
+        return a2(a1Result)
+    }
+}
--- Tests/TersePGTests/TersePGTests.swift
+++ Tests/TersePGTests/TersePGTests.swift
@@ -1,12 +1,39 @@
 import XCTest
 @testable import TersePG
 
-final class TersePGTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+postfix operator *
+infix operator |: AdditionPrecedence
+infix operator >: MultiplicationPrecedence
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+final class TersePGTests: XCTestCase {
+    func testPartialJSONParse() throws {
+        // TODO: On `nil`, return character of failure?
+        // TODO: ? quantifier
+        // TODO: Option to pass recursive matcher to subrules to shorten rule length
+        // Json matcher
+        let V = P("\"") > P("1")* > P("\"")
+        //let K = R { r, input in (V > P(":") > V | V > P(":") > r)(input) }
+        //let K = V > P(":") > V
+        let K = R { r, input in (V > P(":") > V | V > P(":") > P("{") > r > P("}"))(input) }
+        let JR = R { r, input in ((K > P(","))* > K)(input) }
+        //let JR = R { r, input in (K | ((r > P(","))* > r))(input) }
+        //let JR = R { r, input in (V > P(":") > V | V > P(":") > r | (r > P(","))* > r)(input) }
+        let J = P("{") > JR > P("}")
+
+        //V("\"1111\"")
+
+        // Intentionally wrong
+        XCTAssert(J(#"{"111":"a"}"#) == nil)
+        // Correct json
+        XCTAssert(J(#"{"111":"1"}"#)?.isEmpty ?? false)
+        XCTAssert(J(#"{"111":"1","1":"1"}"#)?.isEmpty ?? false)
+        XCTAssert(J(#"{"111":"1","1":{"11":"1"}}"#)?.isEmpty ?? false)
+    }
+    
+    func testNestedListParse() throws {
+        let digit = P("1")
+        let L = R { r, input in (digit|(((P("[") > ((r>P(","))*)) > r) > P("]")))(input) }
+
+        XCTAssert(L("[1,[1,[1],[1]]]")?.isEmpty ?? false)
     }
 }
